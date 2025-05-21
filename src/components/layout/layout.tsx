dfcb8817ff19ef99725822edaee6c023c1bebef5
lucasbatistaa <lucas.lucas2004.30@gmail.com>
2025-05-21 10:23:26 -0300

    feat: hero-section component and add style guide

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Header } from "../header";
 import { Footer } from "../footer";
+import { Inter } from "next/font/google";
 
 type LayoutProps = {
     children: React.ReactNode
 }
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function Layout({ children }: LayoutProps) {
     return (
-        <div className="relative flex min-h-screen flex-col dark">
+        <div className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}>
             <Header />
 
             <main className="flex-1 flex flex-col mb-12">
