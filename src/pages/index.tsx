f08f92b502af30967b985eee23eb0971b17b7fd7
lucasbatistaa <lucas.lucas2004.30@gmail.com>
2025-05-21 15:48:03 -0300

    feat: feature component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { HeroSection } from "@/components/hero-section";
+import { FeatureSection } from "@/components/feature-section";
 
 export default function Home() {
   return (
     <>
-      <HeroSection />
+
+      <article className="flex flex-col">
+        <HeroSection />
+
+        <FeatureSection />
+      </article>
     </>
   );
 }
